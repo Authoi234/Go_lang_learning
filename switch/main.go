6cb5a2d6d60b876db9cd99932b4a7f38c2c90fc0
Authoi234 <jawadkhan.authoi@gmail.com>
2026-06-03 03:16:50 +0600

    switch done

---
+++ switch/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	day := 3
+	switch day {
+	case 1:
+		fmt.Println("Saturday")
+	case 2:
+		fmt.Println("Sunday")
+	case 3:
+		fmt.Println("Monday")
+	case 4:
+		fmt.Println("Tuesday")
+	case 5:
+		fmt.Println("Wednesday")
+	case 6:
+		fmt.Println("Thursday")
+	case 7:
+		fmt.Println("Friday")
+	default:
+		fmt.Println("Unknown day")
+	}
+
+	month := "January"
+	switch month {
+	case "January", "February", "March":
+		fmt.Println("Winter")
+	case "April", "May", "June":
+		fmt.Println("Spring")
+	default:
+		fmt.Println("Other season")
+	}
+}
